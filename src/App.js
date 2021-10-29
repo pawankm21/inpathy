7034e3a07fda56b77e375e43579242c2ca99e106
pawan <b419039@iiit-bh.ac.in>
2021-10-29 22:04:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Home";
+import Meditate from "./Meditate";
+import Tips from "./Tips";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route path="/meditate" component={Meditate} />
+        <Route path="/tips" component={Tips} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Meditate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Meditate() {
+    return (
+        <div>
+            Meditate
+        </div>
+    )
+}
+
+export default Meditate
